f28864440644ea2dd8efe3548a306e3b7a1cae78
Patrick Crosby <patrick@xblabs.com>
2013-03-12 16:14:35 -0500

    added support for (optional) timestamps

--- python/stathat.py
+++ python/stathat.py
@@ -9,15 +9,27 @@ class StatHat:
                 resp = urllib2.urlopen(req)
                 return resp.read()
 
-        def post_value(self, user_key, stat_key, value):
-                return self.http_post('/v', {'key': stat_key, 'ukey': user_key, 'value': value})
+        def post_value(self, user_key, stat_key, value, timestamp=None):
+                args = {'key': stat_key, 'ukey': user_key, 'value': value}
+                if timestamp is not None:
+                        args['t'] = timestamp
+                return self.http_post('/v', args)
 
-        def post_count(self, user_key, stat_key, count):
-                return self.http_post('/c', {'key': stat_key, 'ukey': user_key, 'count': count})
+        def post_count(self, user_key, stat_key, count, timestamp=None):
+                args = {'key': stat_key, 'ukey': user_key, 'count': count}
+                if timestamp is not None:
+                        args['t'] = timestamp
+                return self.http_post('/c', args)
 
-        def ez_post_value(self, ezkey, stat_name, value):
-                return self.http_post('/ez', {'ezkey': ezkey, 'stat': stat_name, 'value': value})
+        def ez_post_value(self, ezkey, stat_name, value, timestamp=None):
+                args = {'ezkey': ezkey, 'stat': stat_name, 'value': value}
+                if timestamp is not None:
+                        args['t'] = timestamp
+                return self.http_post('/ez', args)
 
         def ez_post_count(self, ezkey, stat_name, count):
-                return self.http_post('/ez', {'ezkey': ezkey, 'stat': stat_name, 'count': count})
+                args = {'ezkey': ezkey, 'stat': stat_name, 'count': count}
+                if timestamp is not None:
+                        args['t'] = timestamp
+                return self.http_post('/ez', args)
 
