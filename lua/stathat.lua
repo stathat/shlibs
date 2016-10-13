5335165d8e5028529fe607f6d5d77886b03ae993
skwerlman <skwerlman@users.noreply.github.com>
2016-10-13 03:09:24 -0400

    Update stathat.lua for lua 5.1+
    also update link to luasocket

--- lua/stathat.lua
+++ lua/stathat.lua
@@ -1,20 +1,23 @@
-module(..., package.seeall);
-
--- can download this package at http://w3.impa.br/~diego/software/luasocket/home.html#download
+-- can download this package at https://github.com/diegonehab/luasocket
+-- or via luarocks: luarocks install luasocket
 local http = require("socket.http")
 
-function ez_count(ezkey, stat_name, count)
-        return http.request("http://api.stathat.com/ez", "ezkey=" .. ezkey .. "&stat=" .. stat_name .. "&count=" .. count)
+local stathat = {}
+
+function stathat.ez_count(ezkey, stat_name, count)
+        return http.request("http://api.stathat.com/ez", ("ezkey=%s&stat=%s&count=%s"):format(ezkey, stat_name, count)
 end
 
-function ez_value(ezkey, stat_name, value)
-        return http.request("http://api.stathat.com/ez", "ezkey=" .. ezkey .. "&stat=" .. stat_name .. "&value=" .. value)
+function stathat.ez_value(ezkey, stat_name, value)
+        return http.request("http://api.stathat.com/ez", ("ezkey=%s&stat=%s&value=%s"):format(ezkey, stat_name, value)
 end
 
-function count(stat_key, user_key, count)
-        return http.request("http://api.stathat.com/c", "key=" .. stat_key .. "&ukey=" .. user_key .. "&count=" .. count)
+function stathat.count(stat_key, user_key, count)
+        return http.request("http://api.stathat.com/c", ("key=%s&ukey=%s&count=%s"):format(stat_key, user_key, count)
 end
 
-function value(stat_key, user_key, value)
-        return http.request("http://api.stathat.com/v", "key=" .. stat_key .. "&ukey=" .. user_key .. "&value=" .. value)
+function stathat.value(stat_key, user_key, value)
+        return http.request("http://api.stathat.com/v", ("key=%s&ukey=%s&value=%s"):format(stat_key, user_key, value)
 end
+
+return stathat
